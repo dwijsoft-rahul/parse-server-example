4c63b0ac7a218f2abafbca1051c22f290306cf62
Rahul Patel <rahul@dwijsoft.com>
2018-06-06 16:47:06 -0400

    -- for gamescore

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.afterSave("Messages", function(request) {
+Parse.Cloud.afterSave("GameScore", function(request) {
   console.log('###-----Begin-----###');
   console.log(request);
 });
