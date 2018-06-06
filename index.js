6836a92e46914790beef6911eaed4430269630fe
Rahul Patel <rahul@dwijsoft.com>
2018-06-06 15:02:28 -0400

    -- added config variables

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
+  verbose: process.env.VERBOSE || false,
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  liveQuery: JSON.parse(process.env.LIVE_QUERY || '{}'),
+  // {
+  //   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  // },
+  push: JSON.parse(process.env.SERVER_PUSH || '{}')
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
