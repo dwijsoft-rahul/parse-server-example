e3a04db256601dad04c5889683184063d8a37193
Rahul Patel <rahul@dwijsoft.com>
2018-06-06 16:40:55 -0400

    -- adding aftersave trigger

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,8 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.afterSave("Messages", function(request) {
+  console.log('###-----Begin-----###');
+  console.log(request);
+});
