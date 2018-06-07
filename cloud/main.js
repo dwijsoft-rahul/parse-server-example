8f013efb799f7318dde4006348bbb9575aab3329
Rahul Patel <rahul@dwijsoft.com>
2018-06-07 19:34:00 -0400

    -- after save for message send

--- cloud/main.js
+++ cloud/main.js
@@ -7,3 +7,22 @@ Parse.Cloud.afterSave("GameScore", function(request) {
   console.log('###-----Begin-----###');
   console.log(request);
 });
+
+Parse.Cloud.afterSave("Messages", function(request){
+  var messageText = request.object.get('msg');
+  var usersReceived = request.object.get('tos');
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.containedIn('user', usersReceived);
+  //pushQuery.notEqualTo('user', request.user);
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: "New message: " + messageText
+    }
+  }, { useMasterKey: true}).then(() => {
+      // Push was successful
+      console.log('weeee!');
+  }, (e) => {
+      console.log(e);
+  });
+});
